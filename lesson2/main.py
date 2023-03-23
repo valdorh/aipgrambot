2af32426c9c6f73d9bd6331a32d25c76e1a5368e
valdorh <doroninva@yandex.ru>
2023-03-23 14:33:44 +0300

    Lesson3 Finish

--- lesson2/main.py
+++ lesson2/main.py
@@ -9,7 +9,19 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
+    await message.answer_sticker('CAACAgIAAxkBAAMZZBwt-3VS0nRaB7VHJlbKuZBzNLAAAjgLAAJO5JlLMrFH0tlPjNAvBA')
     await message.answer(f'{message.from_user.first_name}, Добро пожаловать в мой магазин!')
+
+
+@dp.message_handler(content_types=['sticker'])
+async def check_sticker(message: types.Message):
+    await message.answer(message.sticker.file_id)
+    await bot.send_message(message.from_user.id, message.chat.id)
+
+
+@dp.message_handler(content_types=['document', 'photo'])
+async def forward_message(message: types.Message):
+    await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
 
 @dp.message_handler()
